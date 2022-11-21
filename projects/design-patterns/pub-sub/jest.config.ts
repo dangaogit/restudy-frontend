6bf938277674ef408e742b70c397246cde9062ed
dangaogit <ivedk@foxmail.com>
2022-11-21 23:49:25 +0800

    pub-sub: add register test case

--- projects/design-patterns/pub-sub/jest.config.ts
+++ projects/design-patterns/pub-sub/jest.config.ts
@@ -1,8 +1,9 @@
-import type { Config } from 'jest'
+import type { Config } from 'jest';
 
 const config: Config = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-}
+    verbose: true,
+};
 
-export default config
+export default config;
---
+++ projects/design-patterns/pub-sub/src/main/publisher.impl.ts
@@ -0,0 +1,7 @@
+import { Publisher } from './publisher';
+
+export class PublisherImpl implements Publisher {
+    getId(): string {
+        throw new Error('Method not implemented.');
+    }
+}
