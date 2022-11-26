3bfb248ee8b37a61b9da2921dfcca72e760880e8
dangaogit <ivedk@foxmail.com>
2022-11-26 17:17:29 +0800

    pub-sub: update example

--- projects/design-patterns/pub-sub/src/main/example/app.ts
+++ projects/design-patterns/pub-sub/src/main/example/app.ts
@@ -2,5 +2,19 @@ import { View } from '../view/view';
 import { VNode } from '../vnode/vnode';
 
 window.addEventListener('load', () => {
-    View.createApp({ el: document.body, component: new VNode('h1', 'Hello View.') });
+    View.createApp({
+        el: document.body,
+        component: new VNode(
+            'h1',
+            {
+                events: {
+                    click: (e) => {
+                        alert('hello');
+                    },
+                },
+                attrs: { style: 'color: red;' },
+            },
+            'Hello View.'
+        ),
+    });
 });
--- projects/design-patterns/pub-sub/src/main/vnode/vnode.ts
+++ projects/design-patterns/pub-sub/src/main/vnode/vnode.ts
@@ -1,10 +1,29 @@
+export interface VNodeProps {
+    events: Record<string, EventListener>;
+    attrs: Record<string, string>;
+}
+
 export class VNode {
-    public constructor(private readonly tag: string, private readonly children: string | VNode | VNode[]) {}
+    public constructor(
+        private readonly tag: string,
+        private readonly props: VNodeProps,
+        private readonly children: string | VNode | VNode[]
+    ) {}
 
     public toInstance(): Node {
         const el = document.createElement(this.tag);
         if (typeof this.children === 'string') {
             el.innerText = this.children;
+        } else if (this.children instanceof VNode) {
+            el.appendChild(this.children.toInstance());
+        }
+
+        for (const [key, value] of Object.entries(this.props.attrs)) {
+            el.setAttribute(key, value);
+        }
+
+        for (const [key, value] of Object.entries(this.props.events)) {
+            el.addEventListener(key, value);
         }
         return el;
     }
